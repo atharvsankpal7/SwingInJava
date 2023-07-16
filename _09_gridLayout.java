c4f2c17e2adbd0fbe5544462bc976699d3f2af91
Atharv Sankpal <atharvsankpal799@gmail.com>
2023-07-16 22:20:46 +0530

    commit

--- _09_gridLayout.java
+++ _09_gridLayout.java
@@ -1,2 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class _09_gridLayout {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("grid Layout");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(new GridLayout(2,4,10,43));
+
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
+        frame.add(new JButton("10"));
+
+
+
+
+        frame.setVisible(true);
+    }
 }
